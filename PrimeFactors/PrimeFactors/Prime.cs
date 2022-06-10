92d13fc238431dc866f9634468be93b45cc69dcd
rohantare03 <rohatare123@gmail.com>
2022-06-10 09:15:36 +0530

    [Rohan] Add : Finding the Prime Factora of the Number

---
+++ PrimeFactors/PrimeFactors/Prime.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimeFactors
+{
+    public class Prime
+    {
+        int Num, i;
+        public void Factors()
+        {
+            Console.WriteLine("Enter a Number");
+            Num = Convert.ToInt32(Console.ReadLine());
+
+            for ( i = 2; Num > 1; i++)
+            {
+                if (Num % i == 0)
+                {
+                    int r = 0;
+                    while ( Num % i == 0)
+                    {
+                        Num /= i;
+                        r++;
+                    }
+                    Console.WriteLine("{0} is a Prime Factor {1} times. ", i, r);
+                }
+            }
+        }
+    }
+}
---
+++ PrimeFactors/PrimeFactors/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace PrimeFactors
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Prime prime = new Prime();
+            prime.Factors();
+        }
+    }
+}
